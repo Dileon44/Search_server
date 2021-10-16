439f0db238e8668667d7f6fce6e4b88d60df9882
Dileon44 <88231309+Dileon44@users.noreply.github.com>
2021-10-17 02:37:38 +0300

    Variant 1

--- remove_duplicates.cpp
+++ remove_duplicates.cpp
@@ -14,17 +14,17 @@ using std::string;
 void RemoveDuplicates(SearchServer& search_server) {
     const map<int, set<string>> documents = search_server.GetDocuments();
     map<set<string>, int> documents_inverting;
-
-    for (auto it = documents.rbegin(); it != documents.rend(); ++it) { // 1. инициализация обратного словаря
+    
+    for (auto it = documents.rbegin(); it != documents.rend(); ++it) { 
         documents_inverting[it->second] = it->first;
     }
-
+    
     map<int, set<string>> documents_time;
-    for (const auto [set_int, id] : documents_inverting) { // 2. инициализация временного словаря
-        documents_time[id] = set_int;
+    for (const auto [set_words, id] : documents_inverting) {
+        documents_time[id] = set_words;
     }
-
-    for (const auto [id, set_int] : documents) { // 3. удаление и вывод документов
+    
+    for (const auto [id, _] : documents) {
         if (!documents_time.count(id)) {
             cout << "Found duplicate document id " << id << endl;
             search_server.RemoveDocument(id);
